5cb0468224b818821c07227c7ecea132b7765bf8
agent88 <thehidychan@gmail.com>
2018-06-07 22:51:08 +0000

    Error debugged \& fixed

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
-#!/bin/bash                      
+#!/bin/bash
 
 #########################################
-##   AWS EC2 Instance Install Script   ##
+## AWS EC2 Instance Install Script ##
 #########################################
-##     by zer0Bytes                    ##
+## by zer0Bytes##
 #########################################
 
 source ./bash.d/linkdot
 linkdots all
 
+if [ -d ./install.d ]; then
+    for i in ./install.d/*; do
+        [ -f "${i}" ] && source "${i}"
+    done
+fi
+
+
+
+}
